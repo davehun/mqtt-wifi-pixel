4977b1c26fc4cf399fa0ce5fc6809b454c8ab146
David Hunnisett <david@inferiorplanet.co.uk>
2016-05-18 18:08:59 +0100

    blank the details

--- settings.lua
+++ settings.lua
@@ -1,6 +1,6 @@
-ap="***REMOVED***"
-ap_pass="gweld***REMOVED***"
-mqtt_server="192.168.254.10"
+ap=""
+ap_pass=""
+mqtt_server=""
 mqtt_port=1883
 mqtt_client_id="night light"
 mqtt_user_name="user"
